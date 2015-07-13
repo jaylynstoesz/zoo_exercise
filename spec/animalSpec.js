c6cd1d129f901f7a6a5bbc8ebde74a85f7507052
Jaylyn Stoesz <jaylynstoesz@gmail.com>
2015-07-13 11:10:45 -0600

    exercise 1

--- spec/animalSpec.js
+++ spec/animalSpec.js
@@ -10,45 +10,45 @@ describe('Animal', function(){
 
   describe('#oink', function(){
     it('should oink if it is a pig', function(){
-
+      expect(animal.oink()).toEqual("oink");
     });
     it('should not oink if it is not pig', function(){
-
+      animal.kind = "Lion";
+      expect(animal.oink()).toEqual("not a pig");
     });
   });
 
   describe('#growUp', function(){
     it('should increment the age by one', function(){
-
+      expect(animal.growUp()).toEqual(animal.age);
     });
   });
 
   describe('#awake', function(){
     it('should initially be off', function(){
-
+      expect(animal.awake).toEqual(false);
     });
   });
 
    describe('#wakeUp', function(){
     it('should change state to awake', function(){
-
+      expect(animal.wakeUp()).toEqual(true);
     });
   });
 
   describe('#sleep', function(){
     it('should initially be sleeping', function(){
-
+      expect(animal.sleep).toEqual(true);
     });
   });
 
   describe('#feed', function(){
     it('should get fed if awake', function(){
-
+      animal.wakeUp();
+      expect(animal.feed()).toEqual(true);
     });
-    it('not should get fed if sleeping', function(){
-
+    it('should not get fed if sleeping', function(){
+      expect(animal.feed()).toEqual(false);
     });
   });
 });
-
-
--- src/animal.js
+++ src/animal.js
@@ -1,9 +1,36 @@
-function Animal(){
-
+function Animal(name, age, kind){
+  this.name = name;
+  this.age = age;
+  this.kind = kind;
+  this.awake = false;
+  this.sleep = true;
 }
 
 Animal.prototype.oink = function(){
+  if (this.kind === "Pig") {
+    return "oink";
+  } else {
+    return "not a pig";
+  }
+};
 
+Animal.prototype.growUp = function() {
+  this.age += 1;
+  return this.age;
+};
+
+Animal.prototype.wakeUp = function() {
+  this.awake = true;
+  return this.awake;
+};
+
+Animal.prototype.feed = function() {
+  if (this.awake === true) {
+    this.feed = true;
+  } else {
+    this.feed = false;
+  }
+  return this.feed;
 };
 
 module.exports=Animal;
