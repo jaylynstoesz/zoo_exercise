6de400706d322f910f50aa1f0e75f909178c3ceb
Jaylyn Stoesz <jaylynstoesz@gmail.com>
2015-07-13 16:03:36 -0600

    part 2 exercise 1

--- spec/zooSpec.js
+++ spec/zooSpec.js
@@ -14,55 +14,65 @@ describe('Zoo', function(){
 
   describe('#changeLocation', function(){
     it('should change locations', function(){
-
+      expect(zoo.changeLocation("Denver")).toEqual("Denver");
     });
   });
 
   describe('#open', function(){
     it('should change status to open', function(){
-
+      expect(zoo.open()).toEqual(true);
     });
   });
 
 
   describe('#isOpen', function(){
     it('should see if the zoo is open', function(){
-
+      zoo.open();
+      expect(zoo.isOpen()).toEqual(true);
     });
     it('should see if the zoo is closed', function(){
-
+      expect(zoo.isOpen()).toEqual(false);
     });
   });
 
   describe('#animals', function(){
     it('should initially be empty', function(){
-
+      expect(zoo.animals).toEqual([]);
     });
   });
 
 
   describe('#addAnimal', function(){
     it('should only add an animal to the animals array when the zoo is open', function(){
-
+      animal = lion;
+      expect(zoo.addAnimal(animal)).toEqual("No can do.");
     });
-    it('should add an animal to the animals array', function(){
 
+    it('should add an animal to the animals array', function(){
+      zoo.open();
+      animal = lion;
+      expect(zoo.addAnimal(animal)).toEqual([animal]);
     });
 
     it('should only add instances of animals', function(){
-
+      zoo.open();
+      animal = zoo;
+      expect(zoo.addAnimal(animal)).toEqual("No can do.");
     });
 
     it('should not add duplicates', function(){
-
+      zoo.open();
+      zoo.addAnimal(lion);
+      expect(zoo.addAnimal(lion)).toEqual("No can do.");
     });
   });
 
   describe('#removeAnimal', function(){
     it('should remove an animal from the animals array if the zoo is open', function(){
-
+      zoo.open();
+      zoo.addAnimal(lion);
+      zoo.addAnimal(pig);
+      expect(zoo.removeAnimal(lion)).toEqual(null);
     });
   });
 });
-
-
--- src/zoo.js
+++ src/zoo.js
@@ -4,10 +4,56 @@ var Animal = require("./animal");
 function Zoo(name, location){
   this.name = name;
   this.location = location;
+  this.status = false;
+  this.animals = [];
 }
 
 Zoo.prototype.changeLocation = function(newLocation){
+  this.location = newLocation;
+  return this.location;
+};
 
+Zoo.prototype.open = function() {
+  this.status = true;
+  return this.status;
+};
+
+Zoo.prototype.isOpen = function() {
+  if (this.status === false) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+Zoo.prototype.addAnimal = function(animal) {
+  var duplicates = 0;
+  for (var i = 0; i < this.animals.length; i++) {
+    if (this.animals[i].name === animal.name) {
+      duplicates += 1;
+    }
+  }
+  if (duplicates === 0) {
+    if (this.status === true && animal instanceof Animal) {
+      this.animals.push(animal);
+      return this.animals;
+    } else {
+      return "No can do.";
+    }
+  } else {
+    return "No can do.";
+  }
+};
+
+Zoo.prototype.removeAnimal = function(animal) {
+  if (this.status === true) {
+    for (var i = 0; i < this.animals.length; i++) {
+      if (this.animals[i].name === animal.name) {
+        this.animals[i] = null;
+        return this.animals[i];
+      }
+    }
+  }
 };
 
 module.exports=Zoo;
